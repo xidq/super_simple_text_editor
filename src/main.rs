40bc264fa697e75e5d0075d82dad6abc329cdb15
xidq <101984347+xidq@users.noreply.github.com>
2025-03-12 15:48:07 +0100

    Update main.rs
    comments

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ fn main() {
     let native_options = eframe::NativeOptions {
 
         viewport: egui::ViewportBuilder::default()
-            // .with_resizable(false)
+            // .with_resizable(false)    
+        // that .with_resizable is not really working on windows, 
+        // i mean even if u wanna keep that one size,
+        // windows forces maximize operations on windows ;((
             .with_inner_size((600.0, 600.0)),
         // renderer: eframe::Renderer::default().egui_wgpu(),
         ..eframe::NativeOptions::default()
